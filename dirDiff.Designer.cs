36722fef4fbaad936a4b8b4b49e0c2bac299dae2
jackhorwell104 <jackhorwell104@hotmail.com>
2020-07-17 21:06:09 +0100

    Initial check-in

---
+++ dirDiff.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace dirDiff_GUI {
+    partial class dirDiff {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folder1Btn = new System.Windows.Forms.Button();
+            this.folder2Btn = new System.Windows.Forms.Button();
+            this.sharedBtn = new System.Windows.Forms.Button();
+            this.checkedBoxFiles = new System.Windows.Forms.CheckedListBox();
+            this.selectAllBtn = new System.Windows.Forms.Button();
+            this.differentBtn = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.moveItemsBtn = new System.Windows.Forms.Button();
+            this.folder1TxtBox = new System.Windows.Forms.TextBox();
+            this.folder2TxtBox = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog3 = new System.Windows.Forms.FolderBrowserDialog();
+            this.itemsLbl = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(534, 109);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dir Diff";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // folder1Btn
+            // 
+            this.folder1Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.folder1Btn.Location = new System.Drawing.Point(445, 104);
+            this.folder1Btn.Name = "folder1Btn";
+            this.folder1Btn.Size = new System.Drawing.Size(75, 23);
+            this.folder1Btn.TabIndex = 1;
+            this.folder1Btn.Text = "Select folder";
+            this.folder1Btn.UseVisualStyleBackColor = true;
+            this.folder1Btn.Click += new System.EventHandler(this.folder1Btn_Click);
+            // 
+            // folder2Btn
+            // 
+            this.folder2Btn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.folder2Btn.Location = new System.Drawing.Point(445, 147);
+            this.folder2Btn.Name = "folder2Btn";
+            this.folder2Btn.Size = new System.Drawing.Size(75, 23);
+            this.folder2Btn.TabIndex = 3;
+            this.folder2Btn.Text = "Select folder";
+            this.folder2Btn.UseVisualStyleBackColor = true;
+            this.folder2Btn.Click += new System.EventHandler(this.folder2Btn_Click);
+            // 
+            // sharedBtn
+            // 
+            this.sharedBtn.Location = new System.Drawing.Point(13, 196);
+            this.sharedBtn.Name = "sharedBtn";
+            this.sharedBtn.Size = new System.Drawing.Size(107, 34);
+            this.sharedBtn.TabIndex = 5;
+            this.sharedBtn.Text = "Find Shared files";
+            this.sharedBtn.UseVisualStyleBackColor = true;
+            this.sharedBtn.Click += new System.EventHandler(this.sharedBtn_Click);
+            // 
+            // checkedBoxFiles
+            // 
+            this.checkedBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedBoxFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkedBoxFiles.FormattingEnabled = true;
+            this.checkedBoxFiles.Location = new System.Drawing.Point(13, 246);
+            this.checkedBoxFiles.Name = "checkedBoxFiles";
+            this.checkedBoxFiles.ScrollAlwaysVisible = true;
+            this.checkedBoxFiles.Size = new System.Drawing.Size(507, 298);
+            this.checkedBoxFiles.TabIndex = 8;
+            // 
+            // selectAllBtn
+            // 
+            this.selectAllBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.selectAllBtn.Location = new System.Drawing.Point(300, 553);
+            this.selectAllBtn.Name = "selectAllBtn";
+            this.selectAllBtn.Size = new System.Drawing.Size(107, 34);
+            this.selectAllBtn.TabIndex = 9;
+            this.selectAllBtn.Text = "Select All";
+            this.selectAllBtn.UseVisualStyleBackColor = true;
+            this.selectAllBtn.Click += new System.EventHandler(this.selectAllBtn_Click);
+            // 
+            // differentBtn
+            // 
+            this.differentBtn.Location = new System.Drawing.Point(126, 196);
+            this.differentBtn.Name = "differentBtn";
+            this.differentBtn.Size = new System.Drawing.Size(107, 34);
+            this.differentBtn.TabIndex = 10;
+            this.differentBtn.Text = "Find Different files";
+            this.differentBtn.UseVisualStyleBackColor = true;
+            this.differentBtn.Click += new System.EventHandler(this.differentBtn_Click);
+            // 
+            // moveItemsBtn
+            // 
+            this.moveItemsBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.moveItemsBtn.Location = new System.Drawing.Point(413, 553);
+            this.moveItemsBtn.Name = "moveItemsBtn";
+            this.moveItemsBtn.Size = new System.Drawing.Size(107, 34);
+            this.moveItemsBtn.TabIndex = 11;
+            this.moveItemsBtn.Text = "Move Selected Items";
+            this.moveItemsBtn.UseVisualStyleBackColor = true;
+            this.moveItemsBtn.Click += new System.EventHandler(this.moveItemsBtn_Click);
+            // 
+            // folder1TxtBox
+            // 
+            this.folder1TxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folder1TxtBox.Location = new System.Drawing.Point(13, 106);
+            this.folder1TxtBox.Name = "folder1TxtBox";
+            this.folder1TxtBox.Size = new System.Drawing.Size(424, 20);
+            this.folder1TxtBox.TabIndex = 1;
+            // 
+            // folder2TxtBox
+            // 
+            this.folder2TxtBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.folder2TxtBox.Location = new System.Drawing.Point(13, 147);
+            this.folder2TxtBox.Name = "folder2TxtBox";
+            this.folder2TxtBox.Size = new System.Drawing.Size(424, 20);
+            this.folder2TxtBox.TabIndex = 2;
+            // 
+            // itemsLbl
+            // 
+            this.itemsLbl.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.itemsLbl.Location = new System.Drawing.Point(382, 208);
+            this.itemsLbl.Name = "itemsLbl";
+            this.itemsLbl.Size = new System.Drawing.Size(138, 22);
+            this.itemsLbl.TabIndex = 15;
+            this.itemsLbl.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(13, 554);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(280, 33);
+            this.progressBar.TabIndex = 16;
+            // 
+            // dirDiff
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 611);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.itemsLbl);
+            this.Controls.Add(this.folder2TxtBox);
+            this.Controls.Add(this.folder1TxtBox);
+            this.Controls.Add(this.moveItemsBtn);
+            this.Controls.Add(this.differentBtn);
+            this.Controls.Add(this.selectAllBtn);
+            this.Controls.Add(this.checkedBoxFiles);
+            this.Controls.Add(this.sharedBtn);
+            this.Controls.Add(this.folder2Btn);
+            this.Controls.Add(this.folder1Btn);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(550, 650);
+            this.Name = "dirDiff";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DirDiff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button folder1Btn;
+        private System.Windows.Forms.Button folder2Btn;
+        private System.Windows.Forms.Button sharedBtn;
+        private System.Windows.Forms.CheckedListBox checkedBoxFiles;
+        private System.Windows.Forms.Button selectAllBtn;
+        private System.Windows.Forms.Button differentBtn;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button moveItemsBtn;
+        private System.Windows.Forms.TextBox folder1TxtBox;
+        private System.Windows.Forms.TextBox folder2TxtBox;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog3;
+        private System.Windows.Forms.Label itemsLbl;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ dirDiff.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.ComponentModel;
+
+namespace dirDiff_GUI {
+    public partial class dirDiff : Form {
+        // Variables to store location of the selected folders
+        string folder1;
+        string folder2;
+        // Variables to store the number of items found
+        int itemsShare = 0;
+        int itemsDiff = 0;
+        // Variable to hold the directory name to move the files to
+        string moveTo = "";
+        // Boolean to detect weather currently looking for shared or different files
+        bool shared;
+        // Determines how many files have been copied
+        int current;
+        // Calculates how far into the operation the program is
+        int progress;
+        // Variables to help update the UI
+        private readonly SynchronizationContext synchronizationContext;
+        private DateTime previousTime = DateTime.Now;
+        // Determines if a button should say "Select all" or "Deselect all"
+        bool allSelected = true;
+
+        public dirDiff() {
+            InitializeComponent();
+            synchronizationContext = SynchronizationContext.Current; //context from UI thread
+            itemsLbl.Text = "Total items: 0";
+        }
+
+        public void sharedOrDifference(bool shared) {
+            // Resets counters
+            itemsShare = 0;
+            itemsDiff = 0;
+
+            // Relabels the text on the button
+            selectAllBtn.Text = "Select All";
+
+            // Checks that boxes have a value, otherwise it will not run
+            if (folder1TxtBox.Text == "" || folder2TxtBox.Text == "") {
+                MessageBox.Show("Oh no... Not all fields are valid!", "Missing information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Clears checked list box
+            checkedBoxFiles.Items.Clear();
+
+            // Sets value for the location of files
+            folder1 = folder1TxtBox.Text;
+            folder2 = folder2TxtBox.Text;
+
+            // Determines length of folder path to remove it later
+            int folderLength1 = folder1.Length + 1;
+            int folderLength2 = folder2.Length + 1;
+
+            // Generates array of all the files in a given directory
+            string[] filesInFolder1 = Directory.GetFiles(folder1);
+            string[] filesInFolder2 = Directory.GetFiles(folder2);
+
+            // Removes path from all files for comparison
+            for (int i = 0; i < filesInFolder1.Length; i++) {
+                filesInFolder1[i] = filesInFolder1[i].Substring(folderLength1);
+            }
+            for (int i = 0; i < filesInFolder2.Length; i++) {
+                filesInFolder2[i] = filesInFolder2[i].Substring(folderLength2);
+            }
+
+            // Creation of list to store the file names that are shared between both folders
+            List<string> sharedFiles = new List<string>();
+            List<string> differentFiles = new List<string>();
+
+            // Exits function if there are no files in folder1
+            if (filesInFolder1.Length == 0) {
+                MessageBox.Show("Oh no... Folder does not contain any files!", "Empty folder", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Automatically adds all items into differentFiles if no files in folder2
+            else if (filesInFolder2.Length == 0) {
+                foreach (string file in filesInFolder1) {
+                    differentFiles.Add(file);
+                    itemsDiff++;
+                }
+            }
+            else {
+                // Goes through each file in folder 1 and compares it to each file in
+                // folder 2. If they have the same name then it will be added to
+                // sharedFiles and the loop will break, if a file gets to the end of
+                // the files in folder 2 then it will be marked different and added to differentFiles
+                for (int file1 = 0; file1 < filesInFolder1.Length; file1++) {
+                    for (int file2 = 0; file2 < filesInFolder2.Length; file2++) {
+                        if (filesInFolder1[file1] == filesInFolder2[file2]) {
+                            sharedFiles.Add(filesInFolder1[file1]);
+                            itemsShare += 1;
+                            break;
+                        }
+                        else if (file2 == filesInFolder2.Length - 1) {
+                            differentFiles.Add(filesInFolder1[file1]);
+                            itemsDiff += 1;
+                        }
+                    }
+                }
+            }
+
+            // Either does the actions for if the shared button was pressed or the difference button
+            if (shared) {
+                // Adds items to checked list box from sharedFiles then prints how many items were found
+                checkedBoxFiles.Items.AddRange(sharedFiles.ToArray());
+                itemsLbl.Text = $"Total items: {itemsShare}";
+            }
+            else {
+                // Adds items to checked list box from differentFiles then prints how many items were found
+                checkedBoxFiles.Items.AddRange(differentFiles.ToArray());
+                itemsLbl.Text = $"Total items: {itemsDiff}";
+            }
+        }
+
+        // Updates UI at regular intervals
+        public void UpdateUI(int progress = 50) {
+            var timeNow = DateTime.Now;
+
+            // Refresh every 50ms 
+            if ((DateTime.Now - previousTime).Milliseconds <= 50) {
+                return;
+            }
+
+            // Send the update to UI thread  
+            synchronizationContext.Post(new SendOrPostCallback(o => {
+                progressBar.Value = (int)o;
+                checkedBoxFiles.Items.Remove(0);
+            }), progress);
+
+            previousTime = timeNow;
+        }
+
+        private void folder1Btn_Click(object sender, EventArgs e) {
+            // Opens folder browser dialog and allows user to select a directory path
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                folder1TxtBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void folder2Btn_Click(object sender, EventArgs e) {
+            // Opens folder browser dialog and allows user to select a directory path
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK) {
+                folder2TxtBox.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void sharedBtn_Click(object sender, EventArgs e) {
+            // Calls function to find items that are in both folders
+            shared = true;
+            sharedOrDifference(shared);
+        }
+
+        private void differentBtn_Click(object sender, EventArgs e) {
+            // Calls function to find items that are in folder1 but not folder2
+            shared = false;
+            sharedOrDifference(shared);
+        }
+
+        private void selectAllBtn_Click(object sender, EventArgs e) {
+            // Calls function to check if the program should select all items
+            // or delelect all items
+            selectDeselect();
+        }
+
+        private void selectDeselect() {
+            // Determines weather program should select all items or deselect
+            // all items
+            allSelected = false;
+
+            if (checkedBoxFiles.CheckedItems.Count == checkedBoxFiles.Items.Count) {
+                selectAllBtn.Text = "Select All";
+                allSelected = true;
+            }
+            else {
+                selectAllBtn.Text = "Deselect All";
+                allSelected = false;
+            }
+
+            // Selects all of the items in the checkListBox
+            for (int i = 0; i < checkedBoxFiles.Items.Count; i++) {
+                checkedBoxFiles.SetItemChecked(i, !allSelected);
+            }
+        }
+
+        private void moveItemsBtn_Click(object sender, EventArgs e) {
+            // List of which files are selected
+            List<String> selected = new List<string>();
+
+            // Iterates through each item in the check list box and adds the value
+            // to a list 
+            for (int i = 0; i < checkedBoxFiles.Items.Count; i++) {
+                if (checkedBoxFiles.GetItemChecked(i)) {
+                    selected.Add(checkedBoxFiles.Items[i].ToString());
+                }
+            }
+
+            // Opens folder browser dialog and allows user to select a target folder
+            if (folderBrowserDialog3.ShowDialog() == DialogResult.OK) {
+                moveTo = folderBrowserDialog3.SelectedPath;
+            }
+
+            // Values to determine how the progress nar should look
+            current = 0;
+            progressBar.Value = 0;
+
+            var backgroundTask = new BackgroundWorker();
+            backgroundTask.DoWork += (s, f) => {
+                foreach (string file in selected) {
+                    // Checks that the file does not already exists
+                    // Passes directory location then concatenates the file name onto it
+                    // Also passes target directory and concatenates file name
+                    if (!File.Exists($"{moveTo}\\{file}")) {
+                        File.Copy($"{folder1}\\{file}", $"{moveTo}\\{file}");
+                    }
+
+                    // Works out value of the progress bar
+                    current += 1;
+                    progress = (int)((float)current / selected.Count * 100);
+
+                    // Performs UI update every 50ms
+                    UpdateUI(progress);
+                }
+            };
+            backgroundTask.RunWorkerAsync();
+
+            // Clears area to show that it has finished copying files
+            // Also sets progress value and total items to zero
+            checkedBoxFiles.Items.Clear();
+            progressBar.Value = 0;
+            itemsLbl.Text = "Total items: 0";
+        }
+    }
+}
